6b41c1f97cba0d4790432b55d678ac5105fc8260
tdoulg01 <49614554+tdoulg01@users.noreply.github.com>
2024-04-01 19:31:39 +0300

    Update gui.py

--- Project1/gui.py
+++ Project1/gui.py
@@ -1,8 +1,10 @@
+from _ast import match_case
+
 import functions
 import PySimpleGUI
 PySimpleGUI.theme('DarkPurple4')
 label = PySimpleGUI.Text("Type in a to-do")
-input_box = PySimpleGUI.InputText(tooltip="Enter todo")
+input_box = PySimpleGUI.InputText(tooltip="Enter todo", key='todo')
 add_button=PySimpleGUI.Button("Add")
 edit_button = PySimpleGUI.Button("Edit")
 complete_button= PySimpleGUI.Button("Button")
@@ -12,5 +14,13 @@ todos=functions.get_todos()
 window = PySimpleGUI.Window('ToDo List', layout=[[label],[input_box, add_button],
                             [PySimpleGUI.Listbox(values=todos,font='helvetica',size=10),[edit_button],[complete_button]],
                             [exit_button]])
-window.read()
+
+while True:
+event,values=window.read()
+print(event)
+print(values)
+match event:
+    case 'Add':
+        todos=functions.get_todos();
+        
 window.close()
