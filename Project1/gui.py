e40cfbda761d58a6997d4f757037f5877a6ae5f5
tdoulg01 <49614554+tdoulg01@users.noreply.github.com>
2024-04-01 14:16:21 +0300

    Update gui.py

--- Project1/gui.py
+++ Project1/gui.py
@@ -1,10 +1,16 @@
-from functions import *
+import functions
 import PySimpleGUI
 PySimpleGUI.theme('DarkPurple4')
 label = PySimpleGUI.Text("Type in a to-do")
 input_box = PySimpleGUI.InputText(tooltip="Enter todo")
 add_button=PySimpleGUI.Button("Add")
+edit_button = PySimpleGUI.Button("Edit")
+complete_button= PySimpleGUI.Button("Button")
+exit_button = PySimpleGUI.Button("Exit")
+todos=functions.get_todos()
 
-window = PySimpleGUI.Window('ToDo List', layout=[[label],[input_box, add_button]])
+window = PySimpleGUI.Window('ToDo List', layout=[[label],[input_box, add_button],
+                            [PySimpleGUI.Listbox(values=todos,font='helvetica',size=10),[edit_button],[complete_button]],
+                            [exit_button]])
 window.read()
 window.close()
