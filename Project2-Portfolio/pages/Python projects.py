eb2a064cc9e8f1a23586924616c02a6730d5a150
tdoulg01 <49614554+tdoulg01@users.noreply.github.com>
2024-04-03 00:54:09 +0300

    changed python projects page
    made pythong projects page to run faster

--- Project2-Portfolio/pages/Python projects.py
+++ Project2-Portfolio/pages/Python projects.py
@@ -1,20 +1,29 @@
 import streamlit as st
 import pandas
 st.title("Python projects")
+def load_data():
+    return pandas.read_csv("data.csv", sep=";")
 
-col3,empty_col,col4=st.columns([2,0.5,2])
-df = pandas.read_csv("data.csv", sep=";")
+col3,empty_col,col4=st.columns([2.5,0.5,2.5])
+df = load_data()
 
 with col3:
-    for index ,row in df[:10].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/"+row["image"])
-        st.write("[Source Code](https://github.com/Doulgast/Python-projects)")
-
-with col4:
-    for index,row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/"+row["image"])
-        st.write("[Source Code](https://github.com/Doulgast/Python-projects)")
+    for index ,row in df.iterrows():
+        if(index%2==0):
+            with col3:
+                st.header(row["title"])
+                st.write(row["description"])
+                st.image("images/"+row["image"])
+                st.write("[Source Code](https://github.com/Doulgast/Python-projects)")
+        else:
+            with col4:
+                st.header(row["title"])
+                st.write(row["description"])
+                st.image("images/" + row["image"])
+                st.write("[Source Code](https://github.com/Doulgast/Python-projects)")
+# with col4:
+#     for index,row in df[10:].iterrows():
+#         st.header(row["title"])
+#         st.write(row["description"])
+#         st.image("images/"+row["image"])
+#         st.write("[Source Code](https://github.com/Doulgast/Python-projects)")
