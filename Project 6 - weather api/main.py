8e76c6e837481fec56b42fcefdf018eef6d04e9d
tdoulg01 <49614554+tdoulg01@users.noreply.github.com>
2024-04-21 23:47:59 +0300

    finished the weather api

--- Project 6 - weather api/main.py
+++ Project 6 - weather api/main.py
@@ -4,7 +4,8 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("home.html")
+    stations=pd.read_csv("data_small/stations.txt", skiprows=17)
+    return render_template("home.html",data=stations.to_html())
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
     filename=("data_small/TG_STAID"+str(station).zfill(6)+".txt")
@@ -14,7 +15,18 @@ def about(station,date):
     return {"station:":station,
             "date: ": date,
             "temperature:":temperature}
-app.run(debug=True)
-
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = ("data_small/TG_STAID" + str(station).zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station,year):
+    filename = ("data_small/TG_STAID" + str(station).zfill(6) + ".txt")
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 if __name__ == "__main__":
     app.run(debug=True)
